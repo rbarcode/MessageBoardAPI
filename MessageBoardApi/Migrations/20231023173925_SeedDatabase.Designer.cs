31a24c0d6914f1319da6c181c6647645488b1718
onurkaymak <onurkaymak@outlook.com>
2023-10-23 10:41:50 -0700

    Add SeedDatabase migration.
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Richard Barbour <richyb07@gmail.com>

--- MessageBoardApi/Migrations/20231023173925_SeedDatabase.Designer.cs
+++ MessageBoardApi/Migrations/20231023173925_SeedDatabase.Designer.cs
@@ -2,6 +2,7 @@
 using MessageBoardApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MessageBoardApi.Migrations
 {
     [DbContext(typeof(MessageBoardApiContext))]
-    partial class MessageBoardApiContextModelSnapshot : ModelSnapshot
+    [Migration("20231023173925_SeedDatabase")]
+    partial class SeedDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MessageBoardApi/Models/MessageBoardApiContext.cs
+++ MessageBoardApi/Models/MessageBoardApiContext.cs
@@ -13,5 +13,38 @@ namespace MessageBoardApi.Models
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Group>()
+        .HasData(
+          new Group { GroupId = 1, Name = "Spider-Man" },
+          new Group { GroupId = 2, Name = "Witcher" },
+          new Group { GroupId = 3, Name = "Costumes" }
+        );
+
+      builder.Entity<Message>()
+        .HasData(
+          new Message { MessageId = 1, Text = "This new Spider-Man game looks awesome!", GroupId = 1, UserId = 2 },
+          new Message { MessageId = 2, Text = "What did ya'll get for candy? I got rocks.", GroupId = 3, UserId = 1 },
+          new Message { MessageId = 3, Text = "I hate Ciri!", GroupId = 2, UserId = 3 }
+        );
+
+      builder.Entity<User>()
+        .HasData(
+          new User { UserId = 1, Name = "Joey" },
+          new User { UserId = 2, Name = "Richard" },
+          new User { UserId = 3, Name = "Onur" }
+        );
+
+      builder.Entity<GroupUser>()
+        .HasData(
+          new GroupUser { GroupUserId = 1, UserId = 2, GroupId = 1 },
+          new GroupUser { GroupUserId = 2, UserId = 1, GroupId = 3 },
+          new GroupUser { GroupUserId = 3, UserId = 3, GroupId = 2 },
+          new GroupUser { GroupUserId = 4, UserId = 2, GroupId = 2 },
+          new GroupUser { GroupUserId = 5, UserId = 1, GroupId = 2 }
+        );
+    }
+
   }
 }
