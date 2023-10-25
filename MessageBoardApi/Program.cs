707654a5ad37cfedb33e9e419b6893a786972225
Richard Barbour <richyb07@gmail.com>
2023-10-25 10:29:33 -0700

    Refactor Program.cs to add Identity and JWTAuth.

--- MessageBoardApi/Program.cs
+++ MessageBoardApi/Program.cs
@@ -1,14 +1,13 @@
 using MessageBoardApi.Models;
 using Microsoft.EntityFrameworkCore;
-// using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
-
-// THIS VERSION PREVENTS FEEDBACK LOOPS ("OBJECT CYCLES) BUT LEAVES A REFERENCE TO NULL IN PAYLOAD
-// builder.Services.AddControllers().AddJsonOptions(options =>
-//         {
-//           options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-//         });
 
 builder.Services.AddControllers();
 
@@ -20,22 +19,37 @@ builder.Services.AddDbContext<MessageBoardApiContext>(
                     )
                   )
                 );
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MessageBoardApiContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
-else
-{
-  app.UseHttpsRedirection();
-}
+app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
